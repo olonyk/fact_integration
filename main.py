2abed1d79d5009b79c486731df73a500b66de86e
Olov <olonyk@gmail.com>
2017-11-07 16:08:34 +0100

    Possible to launch the modules separatly

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 """
 Launch point for the kernel-server.
+Options:
+    -i                          [Launch Interpreter ]
+    -s                          [Launch Server]
+    -r                          [Launch ROS subscriber]
+    --data=/path/to/data/file   [Set data file]
+Example:
+    python main.py -isr --data=lego/Lego_DB1_shuffle1.csv
 """
 import sys
 from src.kernel import Kernel
 
+
+def _read_command_line(commands, arg):
+    if "--data=" in arg:
+        data_file = arg.split("=")[0]
+    elif "-" in arg:
+        if "i" in arg:
+            commands["interpreter"] = True
+        if "s" in arg:
+            commands["server"] = True
+        if "r" in arg:
+            commands["subscriber"] = True
+    return commands
+
 if __name__ == "__main__":
-    data_base_file = sys.argv[1]
-    kernel = Kernel(data_base_file)
-    kernel.run() 
+    if len(sys.argv) > 1:
+        commands = {"data_file": "lego/Lego_DB1_shuffle1.csv",
+                    "server":False,
+                    "subscriber":False,
+                    "interpreter":False}
+        for arg in sys.argv:
+            commands = _read_command_line(commands, arg)
+        kernel = Kernel(commands)
+        kernel.run()
+    else:
+        print(__doc__)
--- src/kernel.py
+++ src/kernel.py
@@ -5,18 +5,22 @@ import subprocess
 from os.path import realpath, dirname, join
 
 class Kernel(object):
-    def __init__(self, data_base_file):
+    def __init__(self, commands):
         """ Initialize the TCP/IP server, ROS subscriber and the parser. 
         """
-        self.server = Server()
-        self.interpreter = Interpreter(data_base_file)
+        if commands["server"]:
+            self.server = Server()
+        if commands["interpreter"]:
+            self.interpreter = Interpreter(commands["data_file"])
+        self.commands = commands
     
     def run(self):
-        print("Kernel running")
-        self.server.start()
-        time.sleep(3)
-        self.interpreter.start()
-        subscriber = subprocess.call((['python2.7', join(dirname(realpath(__file__)),
-                                                                 "subscriber.py")]))
-        print("Kernel terminating")
+        if self.commands["server"]:
+            self.server.start()
+            # Wait a second so that the server is up before the other processes starts.
+            time.sleep(1)
+        if self.commands["interpreter"]:
+            self.interpreter.start()
+        if self.commands["subscriber"]:
+            subprocess.call((['python2.7', join(dirname(realpath(__file__)), "subscriber.py")]))
     
