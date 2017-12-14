b56d846061ea8214f86459a541e2fb6be9bce0c6
Olov <olonyk@gmail.com>
2017-12-14 17:29:52 +0100

    Added dummy YuMi and dummy Participant
    For developement purposes.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ Options:
     -i                          [Launch Interpreter ]
     -s                          [Launch Server]
     -r                          [Launch ROS subscriber]
-    --data=/path/to/data/file   [Set data file]
+    -da                         [Dummy Architecture]
+    -dy                         [Dummy Yumi]
 Example:
     python main.py -isr --data=lego/Lego_DB1_shuffle1.csv
 """
@@ -13,23 +14,26 @@ from src.kernel import Kernel
 
 
 def _read_command_line(commands, arg):
-    if "--data=" in arg:
-        data_file = arg.split("=")[0]
-    elif "-" in arg:
+    if "-" in arg:
         if "i" in arg:
             commands["interpreter"] = True
         if "s" in arg:
             commands["server"] = True
         if "r" in arg:
             commands["subscriber"] = True
+        if "da" in arg:
+            commands["architecture_dummy"] = True
+        if "dy" in arg:
+            commands["yumi_dummy"] = True
     return commands
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        commands = {"data_file": "lego/Lego_DB1_shuffle1.csv",
-                    "server":False,
+        commands = {"server":False,
                     "subscriber":False,
-                    "interpreter":False}
+                    "interpreter":False,
+                    "architecture_dummy":False,
+                    "yumi_dummy":False}
         for arg in sys.argv:
             commands = _read_command_line(commands, arg)
         kernel = Kernel(commands)
